a55f317d60cb01e9f4af17e8dd720e28c2297105
Pc <vudaotuananh17@gmail.com>
2024-03-19 13:56:35 +0700

    19/3

---
+++ Day1/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    internal class Product
+    {
+        private string name { get; set; }
+        private int price { get; set; }
+        private int quantity { get; set; }
+        private int categoryId { get; set; }
+
+        public Product (string name , int price , int quantity, int categoryId)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity= quantity;
+            this.categoryId = categoryId;
+        }
+        public Product() { }
+
+        public string getName()
+        {
+            return this.name;
+        }
+        public int getPrice()
+        {
+            return this.price;
+        }
+        public int getQuantity()
+        {
+            return this.quantity;
+        }
+        public int getCategoryId()
+        {
+            return this.categoryId;
+        }
+        public void getAll()
+        {
+            Console.WriteLine("Tên sản phẩm: " + name + " Giá sản phẩm: " + price);
+            Console.WriteLine("Số lượng sản phẩm: " + quantity + " Mã danh mục: " + categoryId);
+        }
+
+        public static implicit operator List<object>(Product v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
